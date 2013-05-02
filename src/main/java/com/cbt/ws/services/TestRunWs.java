6304debeee54c6a41f1fafae624cc9628b2b44a7
saulius <noiseoverip@gmail.com>
2013-05-02 23:14:11 +0200

    Add handle fast,normal test run mode
    *Normal is handles the same way as before
    *Fast will distribute test classes among available devices
    *Works only on a unit test level

--- src/main/java/com/cbt/ws/services/TestRunWs.java
+++ src/main/java/com/cbt/ws/services/TestRunWs.java
@@ -1,6 +1,7 @@
 package com.cbt.ws.services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -16,9 +17,13 @@ import org.apache.log4j.Logger;
 import com.cbt.ws.dao.DeviceDao;
 import com.cbt.ws.dao.DevicejobDao;
 import com.cbt.ws.dao.TestRunDao;
+import com.cbt.ws.dao.TestScriptDao;
 import com.cbt.ws.entity.Device;
 import com.cbt.ws.entity.DeviceJob;
 import com.cbt.ws.entity.TestRun;
+import com.cbt.ws.entity.TestScript;
+import com.cbt.ws.entity.complex.TestRunComplex;
+import com.cbt.ws.jooq.enums.TestprofileMode;
 import com.google.inject.servlet.RequestScoped;
 
 /**
@@ -31,18 +36,20 @@ import com.google.inject.servlet.RequestScoped;
 @RequestScoped
 public class TestRunWs {
 
-	//private final Logger mLogger = Logger.getLogger(TestRunWs.class);
+	// private final Logger mLogger = Logger.getLogger(TestRunWs.class);
 
 	private TestRunDao mTestrunDao;
 	private DevicejobDao mDevicejobDao;
 	private DeviceDao mDeviceDao;
 	private final Logger mLogger = Logger.getLogger(TestRunWs.class);
+	private TestScriptDao mTestScriptDao;
 
 	@Inject
-	public TestRunWs(TestRunDao dao, DevicejobDao devicejobDao, DeviceDao deviceDao) {
-		mTestrunDao = dao;
+	public TestRunWs(TestRunDao testRunDao, DevicejobDao devicejobDao, DeviceDao deviceDao, TestScriptDao testScripDao) {
+		mTestrunDao = testRunDao;
 		mDevicejobDao = devicejobDao;
 		mDeviceDao = deviceDao;
+		mTestScriptDao = testScripDao;
 	}
 
 	/**
@@ -57,16 +64,93 @@ public class TestRunWs {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public TestRun add(TestRun testRun) {
-
+		// TODO: move logic to dedicated class
 		// TODO: implement authentication
 		testRun.setUserId(1L);
 
 		Long testRunId = mTestrunDao.add(testRun);
 		testRun.setId(testRunId);
-		
-		List<Long> deviceTypes = mTestrunDao.getTestRunComplex(testRun.getId()).getDeviceTypes();		
-		List<DeviceJob> jobs = new ArrayList<DeviceJob>();		
-		
+
+		TestRunComplex testRunComplex = mTestrunDao.getTestRunComplex(testRun.getId());
+		if (testRunComplex.getTestProfile().getMode().equals(TestprofileMode.NORMAL)) {
+			handleNormalMode(testRunComplex);
+		} else {
+			handleFastMode(testRunComplex);
+		}
+
+		return testRun;
+	}
+	
+	/**
+	 * Handle fast mode. Handles cases:
+	 * when testClasses > devices
+	 * when devices > testClases
+	 * when testClasses = devices
+	 * 
+	 * Workarounds: last device gets the rest of test cases
+	 * 
+	 * @param testRunComplex
+	 */
+	private void handleFastMode(TestRunComplex testRunComplex) {
+
+		List<DeviceJob> jobs = new ArrayList<DeviceJob>();
+		List<Long> deviceTypes = testRunComplex.getDeviceTypes();
+		TestScript testScript = mTestScriptDao.getById(testRunComplex.getTestConfig().getTestScriptId());
+		for (Long deviceType : deviceTypes) {
+			List<Device> devices = mDeviceDao.getDevicesOfType(deviceType);
+
+			// Do this for each of device types
+			int numberOfAvailableDevices = devices.size();
+			List<String> testClasses = testScript.getTestClasses();
+			int numberOfTestCases = testClasses.size();
+			int testClassesPerDevice = (int) Math.floor(numberOfTestCases / numberOfAvailableDevices);
+			if (testClassesPerDevice == 0) {
+				testClassesPerDevice = 1;
+			}
+			mLogger.info("TestClassesPerDevice:" + testClassesPerDevice);
+
+			if (null != devices) {
+				int testClassIndex = 0;
+				int deviceIndex = 0;
+				for (Device device : devices) {
+					DeviceJob job = new DeviceJob();
+					job.setDeviceId(device.getId());
+					job.setUserId(device.getUserId());
+					job.setTestRunId(testRunComplex.getId());
+					List<String> testClassesForDevice = new ArrayList<String>();
+					for (int i=0;i<testClassesPerDevice;i++) {
+						testClassesForDevice.add(testClasses.get(testClassIndex));
+						testClassIndex++;
+						if (deviceIndex == (devices.size() - 1)) {
+							// Add all the rest of test classes if this is the last device that we have
+							while(testClassIndex < (numberOfTestCases - 1)) {
+								testClassesForDevice.add(testClasses.get(testClassIndex));
+								testClassIndex++;
+							}
+						}
+					}
+					job.setMetadata(testClassesForDevice.toString());
+					Long deviceJobId = mDevicejobDao.add(job);
+					if (null != deviceJobId) {
+						job.setId(deviceJobId);
+						jobs.add(job);
+					} else {
+						mLogger.error("Could not created job:" + job);
+					}
+					if ((testClassIndex - 1) == (numberOfTestCases -1)) {
+						// Stop looping through devices if we have distributed all the test cases
+						break;
+					}
+					deviceIndex++;
+				}
+			}
+		}
+
+	}
+
+	private void handleNormalMode(TestRunComplex testRunComplex) {
+		List<DeviceJob> jobs = new ArrayList<DeviceJob>();
+		List<Long> deviceTypes = testRunComplex.getDeviceTypes();
 		for (Long deviceType : deviceTypes) {
 			List<Device> devices = mDeviceDao.getDevicesOfType(deviceType);
 			if (null != devices) {
@@ -74,19 +158,17 @@ public class TestRunWs {
 					DeviceJob job = new DeviceJob();
 					job.setDeviceId(device.getId());
 					job.setUserId(device.getUserId());
-					job.setTestRunId(testRun.getId());	
+					job.setTestRunId(testRunComplex.getId());
 					Long deviceJobId = mDevicejobDao.add(job);
 					if (null != deviceJobId) {
 						job.setId(deviceJobId);
 						jobs.add(job);
 					} else {
 						mLogger.error("Could not created job:" + job);
-					}					
+					}
 				}
 			}
 		}
-		
-		return testRun;
 	}
 
 	@GET
---
+++ src/test/java/com/cbt/ws/services/TestRunWsTest.java
@@ -0,0 +1,92 @@
+package com.cbt.ws.services;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.cbt.ws.dao.DeviceDao;
+import com.cbt.ws.dao.DevicejobDao;
+import com.cbt.ws.dao.TestRunDao;
+import com.cbt.ws.dao.TestScriptDao;
+import com.cbt.ws.entity.Device;
+import com.cbt.ws.entity.DeviceJob;
+import com.cbt.ws.entity.TestConfig;
+import com.cbt.ws.entity.TestProfile;
+import com.cbt.ws.entity.TestRun;
+import com.cbt.ws.entity.TestScript;
+import com.cbt.ws.entity.complex.TestRunComplex;
+import com.cbt.ws.jooq.enums.TestprofileMode;
+
+public class TestRunWsTest {
+	private static ArrayList<Long> DEVICETYPES = new ArrayList<Long>() {
+		private static final long serialVersionUID = 2163048026629128105L;
+		{
+			add(1L);
+		}
+	};
+
+	private final Logger logger = Logger.getLogger(TestRunWsTest.class);
+
+	@Test
+	public void addTestRun() {
+		TestRunDao testRunDao = mock(TestRunDao.class);
+		DevicejobDao devicejobDao = mock(DevicejobDao.class);
+		DeviceDao deviceDao = mock(DeviceDao.class);
+		TestScriptDao testScriptDao = mock(TestScriptDao.class);
+		TestRunWs testRunWs = new TestRunWs(testRunDao, devicejobDao, deviceDao, testScriptDao);
+		TestProfile testProfile = new TestProfile();
+		testProfile.setMode(TestprofileMode.FAST);
+		TestConfig testConfig = new TestConfig();
+		TestRunComplex testRunComplex = new TestRunComplex();
+		testRunComplex.setDeviceTypes(DEVICETYPES);
+		testRunComplex.setTestProfile(testProfile);
+		testRunComplex.setTestConfig(testConfig);
+		TestScript testScript = new TestScript();
+		testScript.setTestClasses(Arrays.asList("testClass1", "testClass2", "testClass3", "testClass4", "testClass5",
+				"testClass6", "testClass7", "testClass8"));
+
+		when(testRunDao.getTestRunComplex(anyLong())).thenReturn(testRunComplex);
+		when(testScriptDao.getById(anyLong())).thenReturn(testScript);
+		when(deviceDao.getDevicesOfType(anyLong())).thenAnswer(new Answer<List<Device>>() {
+
+			@Override
+			public List<Device> answer(InvocationOnMock invocation) throws Throwable {
+				Long deviceId = Long.valueOf(invocation.getArguments()[0].toString());
+				List<Device> devices = new ArrayList<Device>();
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				devices.add(new Device());
+				return devices;
+			}
+		});
+
+		when(devicejobDao.add(any(DeviceJob.class))).thenAnswer(new Answer<Long>() {
+
+			@Override
+			public Long answer(InvocationOnMock invocation) throws Throwable {
+				logger.info("Called add devicejob");
+				return null;
+			}
+		});
+		TestRun testRun = new TestRun();
+		testRun.setId(1L);
+		testRunWs.add(testRun);
+	}
+}
